e3daaaec812e32c1acf78bb380af217cb922ec06
Aliibrahim5 <Ali.ibrahim.elq.js@tuwaiq.edu.sa>
2021-11-09 23:31:36 +0300

    problem

---
+++ Count-Odd-Numbers-below-n.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+
+
+
+// The solution
+
+// function oddCount(n){
+//   let num =0;
+  
+//   for(let i =1; i<n;i++){
+//     if(i%2!=0)
+//       num++
+//   }
+//   return num;
+// }
+
+
+
+//Explanation of what is required
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests", () => {
+//    it("Testing for fixed tests", () => {
+//     assert.strictEqual(oddCount(15), 7, "Oops! Wrong.");
+//     assert.strictEqual(oddCount(15023), 7511, "Oops! Wrong.");
+//   });
+// });
